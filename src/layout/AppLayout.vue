94153985136867d567bcd1690e9f66b98227e603
Arthur da Silva dos Santos <asdsantos@sercom.com.br>
2025-02-18 16:42:50 -0300

    feat: componente HeaderBar criado, novas funcionalidades para AppLayout criadas e feitas alterações no css básico do projeto

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -1,11 +1,40 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import HeaderBar from "../components/HeaderBar.vue";
 </script>
 
 <template>
-  <div>
-    <RouterView />
+  <div class="app-layout-container">
+    <div class="content-container">
+      <HeaderBar />
+      <div class="content-view">
+        <RouterView />
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app-layout-container {
+  display: flex;
+  height: 100vh;
+}
+
+.content-container {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  overflow-x: auto;
+}
+
+.content-view {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid black;
+  border-radius: 3px;
+  width: auto;
+  height: 100%;
+  margin: 0 1.5rem 1.5rem 1.5rem;
+  overflow-y: auto;
+}
+</style>
