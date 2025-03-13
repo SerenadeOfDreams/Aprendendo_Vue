49f4ca8ecee6ab8878f82c97c50520f25a257547
Arthur da Silva dos Santos <asdsantos@sercom.com.br>
2025-03-13 17:43:01 -0300

    feat: botão do instagram adicionado

---
+++ src/components/InstagramButton.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { PhInstagramLogo } from "@phosphor-icons/vue";
+</script>
+
+<template>
+  <button class="instagram-button-container">
+    <PhInstagramLogo size="90" />
+  </button>
+</template>
+
+<style scoped>
+.instagram-button-container {
+  width: fit-content;
+  height: fit-content;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  outline: none;
+  background: none;
+  cursor: pointer;
+}
+
+svg {
+  fill: var(--instagram-blackNwhite);
+  transition: var(--theme-transition-time);
+}
+svg:hover {
+  fill: var(--border6);
+}
+</style>
